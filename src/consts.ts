fb0865d1980824669af3bef67cc8a6f10e44d81b
ErickAndrewBF <171943@unsaac.edu.pe>
2026-07-14 21:37:15 -0300

    Primer commit: Portafolio

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Erick Bustamante | Full Stack Developer';
+export const SITE_DESCRIPTION = 'Portafolio de Erick Bustamante, Ingeniero de Software Full Stack y Frontend.';
